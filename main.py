c4b1e71edde0e767db0fefb72667962d68824e0b
eastmanap <eastmanap354@northwested.org>
2024-09-25 10:48:26 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+# Apollos Eastman
+# 9/25/2024
+# simple-math
+
+# variables
+num1 = 3
+num2 = 8
+
+# calculation
+
+sum = num1 + num2
+
+difference = num1 - num2
+
+product = num1 * num2
+
+quotient = num1 / num2
+
+remainder = num1 % num2
+
+# print
+
+print (f'Sum: {sum}')
+print (f'Difference: {difference}')
+print (f'Product: {product}')
+print (f'remainder: {remainder}')
